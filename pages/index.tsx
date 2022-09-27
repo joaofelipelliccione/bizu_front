acca75c55c7cbb57e7c27c700df9ddda8242ef2c
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-09-27 18:36:04 -0300

    Continuando desenvolvimento!

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,10 @@ const Home: NextPage = () => (
     <Typography variant='h4'>
       Hello World!
     </Typography>
-    <Link href="buscar/mobileApps" passHref>
+    <Link href="mobile/apps" passHref>
       <Button variant="contained">Ir p/ Mobile</Button>
     </Link>
-    <Link href="buscar/webApps" passHref>
+    <Link href="web/apps" passHref>
       <Button variant="contained">Ir p/ Web</Button>
     </Link>
   </main>
