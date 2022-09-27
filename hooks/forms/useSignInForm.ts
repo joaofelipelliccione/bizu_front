e24853b93d1fee711e5ea80ea53c1134259a1288
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-09-27 19:49:40 -0300

    Continuando desenvolvimento!

--- hooks/forms/useSignInForm.ts
+++ hooks/forms/useSignInForm.ts
@@ -1,15 +1,36 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { ISignInFormData } from '../../interfaces/users';
 
 function useSignInForm() {
-  const { register, handleSubmit } = useForm<ISignInFormData>();
+  const validationSchema = React.useMemo(() => (
+    yup.object().shape({
+      username: yup
+        .string()
+        .email('Formato de e-mail porra!')
+        .required('E-mail obrigatório'),
+      password: yup
+        .string()
+        .matches(/^((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 'Endireita a senha!')
+        .required('Senha obrigatória'),
+    })
+  ), []);
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ISignInFormData>({
+    resolver: yupResolver(validationSchema),
+  });
+
   const onSubmit = React.useCallback((formValues: ISignInFormData) => {
     console.log(formValues);
+    // CHAMAR API
+    // FAZER O DISPATCH
   }, []);
 
   return {
     register,
+    errors,
     onSubmit: handleSubmit(onSubmit),
   };
 }
--- pages/acessar/conta.tsx
+++ pages/acessar/conta.tsx
@@ -4,13 +4,15 @@ import useSignInForm from '../../hooks/forms/useSignInForm';
 import styles from '../../styles/pages/signIn.module.css';
 
 const Conta: NextPage = () => {
-  const { register, onSubmit } = useSignInForm();
+  const { register, onSubmit, errors } = useSignInForm();
 
   return (
     <main className={styles.signInPage}>
       <form onSubmit={onSubmit}>
         <input {...register('username')} placeholder="e-mail" />
+        <p>{errors.username?.message}</p>
         <input {...register('password')} placeholder="senha" />
+        <p>{errors.password?.message}</p>
         <button type="submit">
         Fazer Login
         </button>
