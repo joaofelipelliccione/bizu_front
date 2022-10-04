ba4397ac0d356f878ce3362aaf0c3e3094c19139
João Felipe P <joaofelipelliccione@JFs-MacBook-Pro.local>
2022-10-04 10:46:04 -0300

    Continuando desenvolvimento!

--- services/bizuAxios.ts
+++ services/bizuAxios.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+// const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://www.api.bizudesign.io';
+
 export default axios.create({
-  baseURL: 'https://www.api.bizudesign.io',
+  baseURL: BASE_URL,
   timeout: 5000,
+  withCredentials: true,
   headers: {
     'Content-type': 'application/json;charset=UTF-8',
   },
