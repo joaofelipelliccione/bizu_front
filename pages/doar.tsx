41ab398aae59a8b94855e0760194b284f1a76085
Alessandra Sayao <alessandrasayao@cladmins-MacBook-Pro.local>
2022-10-13 10:06:18 -0300

    Continuando progresso

--- pages/doar.tsx
+++ pages/doar.tsx
@@ -1,9 +1,11 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { Typography } from '@mui/material';
+import Navbar from '../components/navbar/Navbar';
 import styles from '../styles/pages/donate.module.css';
 
 const Donate: NextPage = () => (
   <main className={styles.donationsPage}>
+    <Navbar />
     <Typography variant='h4'>
       Página de doação
     </Typography>
