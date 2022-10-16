643ea7ce35ced9078f729dffb2531ee1e03f888a
Alessandra Sayao <alessandrasayao@cladmins-MacBook-Pro.local>
2022-10-16 12:51:48 -0300

    Continuando desenvolvimento.

--- components/home/FirstSection.tsx
+++ components/home/FirstSection.tsx
@@ -3,14 +3,38 @@ import { Grid, Typography } from '@mui/material';
 import Img from '../images/Img';
 import logoNubank from '../../assets/homePage/logoNubank.png';
 import logoRappi from '../../assets/homePage/logoRappi.png';
+import logoPicPay from '../../assets/homePage/logoPicPay.png';
+import logoIfood from '../../assets/homePage/logoIfood.png';
+import logoRocketseat from '../../assets/homePage/logoRocketseat.png';
+import logoEnjoei from '../../assets/homePage/logoEnjoei.png';
+import logoZeDelivery from '../../assets/homePage/logoZeDelivery.png';
+import logoReiDoPitaco from '../../assets/homePage/logoReiDoPitaco.png';
 import styles from '../../styles/pages/home.module.css';
 
 const appsLogos = [
   {
-    id: 1, appClass: styles.nubankLogo, appName: 'Nubank', appImgSrc: logoNubank,
+    id: 1, appClass: styles.logoNubank, appName: 'Nubank', appImgSrc: logoNubank,
   },
   {
-    id: 2, appClass: styles.rappiLogo, appName: 'Rappi', appImgSrc: logoRappi,
+    id: 2, appClass: styles.logoRappi, appName: 'Rappi', appImgSrc: logoRappi,
+  },
+  {
+    id: 3, appClass: styles.logoPicPay, appName: 'Pic Pay', appImgSrc: logoPicPay,
+  },
+  {
+    id: 4, appClass: styles.logoIfood, appName: 'ifood', appImgSrc: logoIfood,
+  },
+  {
+    id: 5, appClass: styles.logoRocketseat, appName: 'Rocketseat', appImgSrc: logoRocketseat,
+  },
+  {
+    id: 6, appClass: styles.logoEnjoei, appName: 'Enjoei', appImgSrc: logoEnjoei,
+  },
+  {
+    id: 7, appClass: styles.logoZeDelivery, appName: 'Zé Delivery', appImgSrc: logoZeDelivery,
+  },
+  {
+    id: 8, appClass: styles.logoReiDoPitaco, appName: 'Rei do Pitaco', appImgSrc: logoReiDoPitaco,
   },
 ];
 
@@ -18,6 +42,7 @@ const FirstSection: NextPage = () => (
   <Grid
     container
     className={styles.homePage1stSection}
+    justifyContent={'center'}
   >
     {appsLogos.map(({
       id, appClass, appName, appImgSrc,
