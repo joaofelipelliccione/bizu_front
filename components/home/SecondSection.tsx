605027256a397343da8ee60e44316d44b1ff489f
Alessandra Sayao <alessandrasayao@cladmins-MacBook-Pro.local>
2022-10-16 13:04:01 -0300

    Continuando desenvolvimento.

---
+++ components/home/SecondSection.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import styles from '../../styles/pages/home.module.css';
+
+const SecondSection: NextPage = () => (
+  <section className={styles.homePage2ndSection}>
+    Olá Mundo
+  </section>
+);
+
+export default SecondSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import Navbar from '../components/navbar/Navbar';
 import FirstSection from '../components/home/FirstSection';
+import SecondSection from '../components/home/SecondSection';
 import styles from '../styles/pages/home.module.css';
 
 const Home: NextPage = () => (
   <main className={styles.homePageMain}>
     <Navbar />
     <FirstSection />
+    <SecondSection />
   </main>
 );
 
